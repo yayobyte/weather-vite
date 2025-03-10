31a2015e6031f86d3586c94be30c0621accd9f08
Yayo Gutierrez <yayobyte@gmail.com>
2025-03-10 18:46:04 -0500

    install material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import './App.css'
 import { useWeather } from './hooks/useWeather'
 import SearchBar from './components/SearchBar/SearchBar'
 
 function App() {
-  const [location, setLocation] = useState('Medellin')
-  const { data, isLoading, isError, error } = useWeather(location)
+	const [location, setLocation] = useState('Medellin')
+	const { data, isLoading, isError, error } = useWeather(location)
 
-  const onSearch = (city: string) => {
-    setLocation(city)
-  }
+	const onSearch = (city: string) => {
+		setLocation(city)
+	}
 
-  return (
-    <>
-      <div>
-        <h1>Weather Service</h1>
-        <div>Current Location: <span>{location}</span></div>
-        <SearchBar onSearch={onSearch} />
-        {isLoading && <div>Loading weather data...</div>}
-        {isError && <div>Error: {error?.message}</div>}
-        {data && (
-          <div>
-            <h2>{data.location.name}, {data.location.country}</h2>
-            <p>{data.current.temp_c}°C / <span>{data.current.temp_f}°F</span></p>
-          </div>
-      )}
-      </div>
-    </>
-  )
+	return (
+		<>
+		<div>
+			<h1>Weather Service</h1>
+			<div>Current Location: <span>{location}</span></div>
+			<SearchBar onSearch={onSearch} />
+			{isLoading && <div>Loading weather data...</div>}
+			{isError && <div>Error: {error?.message}</div>}
+			{data && (
+			<div>
+				<h2>{data.location.name}, {data.location.country}</h2>
+				<p>{data.current.temp_c}°C / <span>{data.current.temp_f}°F</span></p>
+			</div>
+		)}
+		</div>
+		</>
+	)
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { theme } from './theme.ts'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>,
+	<StrictMode>
+	<QueryClientProvider client={queryClient}>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</QueryClientProvider>
+	</StrictMode>,
 )
